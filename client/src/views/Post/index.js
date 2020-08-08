71964253b962be552f9bea36a1b409595a422836
hoang <vinhyenvodoi98@gmail.com>
2020-08-08 16:05:36 +0700

    feat : update View an Post

--- client/src/views/Post/index.js
+++ client/src/views/Post/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { makeStyles, Grid, Paper } from '@material-ui/core';
+import { makeStyles, Grid, Paper, Button } from '@material-ui/core';
 import ReactMarkdown from 'react-markdown';
 import SimpleMDE from 'react-simplemde-editor';
 
@@ -26,17 +26,27 @@ export default function Post() {
   return (
     <div className={`${classes.center} ${classes.ch}`}>
       <Grid container spacing={3}>
-        <Grid item xs={6}>
+        <Grid item xs={12} sm={6}>
           <Paper className={`${classes.ch} ${classes.sc}`} elevation={3}>
             <SimpleMDE onChange={setHandleChange} />
           </Paper>
         </Grid>
-        <Grid item xs={6}>
+        <Grid item xs={12} sm={6}>
           <Paper className={`${classes.ch} ${classes.sc}`} elevation={3}>
             <div className={classes.pt}>
               <ReactMarkdown source={handleChange} />
             </div>
           </Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Button variant='outlined' color='primary'>
+            Generate Skynet Link
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          <a href='https://siasky.net/AAASEbywt22yuIzQosBOuuK5UkICAmTtaGdQrYhrF7DOMw'>
+            https://siasky.net/AAASEbywt22yuIzQosBOuuK5UkICAmTtaGdQrYhrF7DOMw
+          </a>
         </Grid>
       </Grid>
     </div>
--- client/src/views/View/index.js
+++ client/src/views/View/index.js
@@ -1,14 +1,71 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core';
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import { makeStyles, Paper, InputBase, IconButton } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    paddingTop: '15vh',
     flexGrow: 1,
+  },
+  pt: {
+    padding: '5px 15px',
+  },
+
+  bt: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: '99%',
+    marginBottom: '30px',
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+  divider: {
+    height: 28,
+    margin: 4,
   },
 }));
 
 export default function View() {
   const classes = useStyles();
+  const input = '# This is a header\n\nAnd this is a paragraph';
+  const [blogUrl, setBlogUrl] = useState('');
 
-  return <div className={classes.root}>hello</div>;
+  const handleSearch = () => {
+    console.log(blogUrl);
+    setBlogUrl('');
+  };
+
+  return (
+    <div className={classes.root}>
+      <Paper className={classes.bt} elevation={3}>
+        <InputBase
+          className={classes.input}
+          placeholder='Your blog url. Example : https://siasky.net/AAB1OOTWRLeI8hDqd0Hmke6jNRwulY57bQKsCcc_2P6_UQ'
+          inputProps={{ 'aria-label': 'search blog url' }}
+          value={blogUrl}
+          onChange={(event) => setBlogUrl(event.target.value)}
+        />
+        <IconButton
+          onClick={() => handleSearch()}
+          className={classes.iconButton}
+          aria-label='search'
+        >
+          <SearchIcon />
+        </IconButton>
+      </Paper>
+
+      <Paper className={`${classes.ch} ${classes.sc}`} elevation={3}>
+        <div className={classes.pt}>
+          <ReactMarkdown source={input} />
+        </div>
+      </Paper>
+    </div>
+  );
 }
